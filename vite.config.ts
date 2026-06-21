dc4cb9944ef08ed7f3d7cddfe5718d5cbb2c3f16
RypAsuka-Akira <942475402@qq.com>
2026-06-21 17:52:44 +0800

    chore: initialize modern-chronicle-cn

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // 部署到 GitHub Pages 的子路径：https://kitee0325.github.io/modern-chronicle/
-  base: '/modern-chronicle/',
+  // 部署到 GitHub Pages 的子路径：https://kitee0325.github.io/modern-chronicle-cn/
+  base: '/modern-chronicle-cn/',
   plugins: [react()],
 })
